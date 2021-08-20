e11cdf0b17e262651043e2e664643e9b0f786c8f
Lucky Angelo Rabosa <angelorabosa55@yahoo.com.ph>
2021-08-20 16:24:37 +0800

    error

--- src/db/database.js
+++ src/db/database.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql2');
 
-const uri = 'mysql://u3nhojkyfzisauwr:nhtMziGpaFlvqnK9yV7s@b2dql3cn1qsogappkey0-mysql.services.clever-cloud.com:3306/b2dql3cn1qsogappkey0'
 
- const connection = mysql.createConnection({
-   uri: uri
+
+var connection = mysql.createConnection({
+   uri: 'mysql://u3nhojkyfzisauwr:nhtMziGpaFlvqnK9yV7s@b2dql3cn1qsogappkey0-mysql.services.clever-cloud.com:3306/b2dql3cn1qsogappkey0'
 })
 
-let pool = mysql.createPool(uri);
-
-pool.on('connection', function (_conn) {
-    if (_conn) {
-        logger.info('Connected the database via threadId %d!!', _conn.threadId);
-        _conn.query('SET SESSION auto_increment_increment=1');
+connection.connect (function (err) {
+    if(err) {
+        console.log('error connecting' + err.stack)
+        return;
     }
-});
 
+    console.log('connected as id' + connection.threadId)
+})
 
 module.exports = connection
+
