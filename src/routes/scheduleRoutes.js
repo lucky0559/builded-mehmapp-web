f8037b4cb056715dbc746385fd89512f7230b619
Lucky Angelo Rabosa <angelorabosa55@yahoo.com.ph>
2021-08-30 11:14:01 +0800

    changes

--- src/routes/scheduleRoutes.js
+++ src/routes/scheduleRoutes.js
@@ -82,7 +82,7 @@ router.put('/edit/:apointment_id', async(req,res) => {
 
         if(response[0][0].time === time && response[0][0].date === date) {
             await db.promise().query(`UPDATE appointment SET user_id = '${user_id}', date = '${date}', time = '${time}', contact_number = '${contact_number}', status = '${status}' WHERE appointment_id = '${app_id}' `)
-            return res.status(200).send("Updated Successfully!")
+            res.status(200).send("Updated Successfully!")
         }
         else if(response[0].length > 0 ) {
             return res.status(400).send({message: 'Schedule not Available'})
