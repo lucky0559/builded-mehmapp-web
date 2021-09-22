1d1180e1afda4628fc723861ca71aa41adb17475
Lucky Angelo Rabosa <angelorabosa55@yahoo.com.ph>
2021-09-22 15:21:06 +0800

    changes

--- src/routes/scheduleRoutes.js
+++ src/routes/scheduleRoutes.js
@@ -16,8 +16,10 @@ router.post('/add', async(req,res) => {
 
                 const phoneNumber = await db.promise().query(`SELECT phoneNumber FROM users WHERE id = '${user_id}' `)
 
+                const number = phoneNumber[0][0]
+
                 await db.promise().query(`INSERT INTO appointment(user_id, date, time, contact_number, status) VALUES('${user_id}', '${date}', '${time}', '${phoneNumber[0][0]}', '${'Waiting'}' ) `)
-                res.status(201).send({msg:"Schedule Added!", contact_number:`${phoneNumber[0][0]}`})
+                res.status(201).send({msg:"Schedule Added!", contact_number:`${number}`})
             }
         }
         catch(err) {
